6369a0332d153e2a36e7b65262a59fd43dee1d0f
Xiaoying Pu <xpu@umich.edu>
2020-11-22 15:21:14 -0500

    package loads and example runs

--- R/animate_group_by_sanddance.R
+++ R/animate_group_by_sanddance.R
@@ -8,40 +8,41 @@
 #' @import dplyr
 #' @import tidyr
 #' @import purrr
+#' @import tweenr
 #' @import rlang
 #' @export
 animate_group_by_sanddance <- function(.data, ..., nframes = 5, is_last = FALSE){
-  
+
   # prep some mapping
   dots <- enquos(...)
   group_vars_chr <- map_chr(dots, ~ quo_name(.))
   color_var_chr <- first(dots)
   grouped_facet_var <- paste(group_vars_chr, collapse =  "_")
   grouped_facet_sym <- sym(grouped_facet_var)
-  
+
   df <- .data
-  
+
   # browser()
   # preprocess data
   .data <- .data %>%
     mutate(self = "id") %>%
-    unite(!!grouped_facet_var, !!group_vars_chr, remove = FALSE) 
-  
+    unite(!!grouped_facet_var, !!group_vars_chr, remove = FALSE)
+
   fct_lvls <- .data %>%
     mutate(!!grouped_facet_var := factor({{ grouped_facet_sym }})) %>%
     pull(!!grouped_facet_var) %>%
     levels()
-  
+
   if (length(fct_lvls) == 4) {
     fct_lvls <- fct_lvls[c(1,3,2,4)]
   }
-  
+
   .data <- .data %>%
     mutate(!!grouped_facet_var := factor(
       {{ grouped_facet_sym}} , levels = fct_lvls
     )) %>%
-    arrange(!!sym(grouped_facet_var)) 
-  
+    arrange(!!sym(grouped_facet_var))
+
   # initial frame
   #   with no color mapping
   init_coords <- .data %>%
@@ -49,31 +50,31 @@ animate_group_by_sanddance <- function(.data, ..., nframes = 5, is_last = FALSE)
 
   # p_init <- plot_grouped_dataframe_sanddance(init_coords)
   # print(p_init)
-  
-  
-  # final frame  
+
+
+  # final frame
   final_coords <- .data %>%
     waffle_iron_groups(
       aes_d(group = !!grouped_facet_var, x = !!grouped_facet_var)) %>%
     ungroup() %>%
     mutate(id = row_number(), time = 2)
-  
+
   # p_final <- plot_grouped_dataframe_sanddance(final_coords)
   # print(p_final)
-  
+
   #  offset the prev step
-  
-  init_coords_offset <- 
-    init_coords %>% 
+
+  init_coords_offset <-
+    init_coords %>%
     # mutate(y = y + max(final_coords$y) + 5) %>%
     ungroup() %>%
     mutate(id = row_number(), time = 1)
-    
-  
+
+
   p_init_offset <- plot_grouped_dataframe_sanddance(init_coords_offset)
   p_final <- plot_grouped_dataframe_sanddance(final_coords)
   # print(p_init_offset)
-  
+
   # limits
   # xlim <- c(0, max(final_coords$x) + 1)
   # ylim <- c(0, max(init_coords_offset$y) + 1)
@@ -83,19 +84,19 @@ animate_group_by_sanddance <- function(.data, ..., nframes = 5, is_last = FALSE)
   xlim_init[2] <- xlim_init[2] + (xlim_init[2] - xlim_init[1])/4
   ylim_init[1] <- ylim_init[1] - (ylim_init[2] - ylim_init[1])/4
   ylim_init[2] <- ylim_init[2] + (ylim_init[2] - ylim_init[1])/4
-  
+
   xlim_final <- layer_scales(p_final)$x$range$range
   ylim_final <- layer_scales(p_final)$y$range$range
   xlim_final[1] <- xlim_final[1] - (xlim_final[2] - xlim_final[1])/4
   xlim_final[2] <- xlim_final[2] + (xlim_final[2] - xlim_final[1])/4
   ylim_final[1] <- ylim_final[1] - (ylim_final[2] - ylim_final[1])/4
   ylim_final[2] <- ylim_final[2] + (ylim_final[2] - ylim_final[1])/4
-  
-  
+
+
   # aesthetics mapping
   aes_with_group <- aes(group = !!grouped_facet_var, color = !!color_var_chr)
-  
-  # tweening between states -> 
+
+  # tweening between states ->
   # states:
   # 1 grey, ungrouped icon array layout
   # 2 colored, ungrouped icon array layout (keep state, but color 'em)
@@ -104,22 +105,22 @@ animate_group_by_sanddance <- function(.data, ..., nframes = 5, is_last = FALSE)
   total_nframes <- 4 * nframes
   if (is_last) {
     total_nframes <- 5 * nframes
-  } 
-  
+  }
+
   tweens <- init_coords_offset %>%
     ungroup() %>%
     bind_rows(init_coords_offset %>% mutate(time = 2, group = final_coords$group) ) %>%
     bind_rows(final_coords %>% mutate(time = 3)) %>%
-    split(.$time) 
-  
-  tweens_df <- tweens$`1` %>% 
+    split(.$time)
+
+  tweens_df <- tweens$`1` %>%
     keep_state(nframes) %>%
     tween_state(tweens$`2`, nframes = 1, ease = "linear") %>%
     keep_state(nframes -1) %>%
     tween_state(tweens$`3`, nframes = nframes, ease = "linear") %>%
     keep_state(ifelse(is_last, nframes * 2, nframes)) %>% # keep the icon array up for a bit
     split(.$.frame)
-  
+
   tween_lims_list <- tribble(
     ~xlim, ~ylim, ~time,
     xlim_init, ylim_init, 1,
@@ -130,7 +131,7 @@ animate_group_by_sanddance <- function(.data, ..., nframes = 5, is_last = FALSE)
     group_by(time) %>%
     mutate(lim_id = row_number()) %>%
     group_split()
-  
+
   tween_lims <- tween_lims_list[[1]] %>%
     keep_state(nframes) %>%
     tween_state(tween_lims_list[[2]], nframes = nframes, ease = "linear")  %>%
@@ -141,20 +142,20 @@ animate_group_by_sanddance <- function(.data, ..., nframes = 5, is_last = FALSE)
     #   id = lim_id, time = time
     # ) %>%
     split(.$.frame)
-  
+
   walk(
     1:(total_nframes), function(i){
-      
+
       df <- tweens_df[[i]]
       lims <- tween_lims[[i]]
       xlim <- lims$xlim
       ylim <- lims$ylim
-      
+
       if (!(i %in% 1:nframes)) {
-        df <- df %>% 
+        df <- df %>%
           group_by(group)
       }
-      
+
       # ACHTUNG: hard-coding stuff for the experiment
       if (length(group_vars_chr) == 1){
         title <- "Step 1: Each dot shows one person\n            and each group shows degree type"
@@ -166,18 +167,18 @@ animate_group_by_sanddance <- function(.data, ..., nframes = 5, is_last = FALSE)
       # natural_group_var_chr <- paste(group_vars_chr, collapse = " AND ")
       # title <- paste0("Step 1: Each dot shows one person\n            and each group shows ", natural_group_var_chr)
 
-      
+
       print(plot_grouped_dataframe_sanddance(
-        df, xlim, ylim, 
+        df, xlim, ylim,
         mapping = aes_with_group,
         title = title
         ))
     }
   )
-  
+
   # return the final coordinates
   # final_coords
-  df %>% 
+  df %>%
     group_by(!!!map(group_vars_chr, sym))
 }
 
@@ -201,25 +202,25 @@ waffle_iron_groups <- function (data, mapping, rows = 7, sample_size = 1, na.rm 
                                                     sample_size))
   data <- data[sample_rows, ]
 
-  
-  
+
+
   group_mapping <- NULL
   # if there's x or y mapping, split up `data`
   if ("x" %in% names(mapping) ){
     x_var <- mapping$x
-    
+
   # recover group type
     group_mapping <- data %>%
       group_by(!!sym(x_var)) %>%
       group_keys() %>%
       rename(real_group = !!sym(x_var)) %>%
       mutate(group = row_number())
-    
+
     data <- data %>%
       group_by(!!sym(x_var), .add = TRUE) %>%
       group_split()
       # group_split(!!sym(x_var)) # assuming x_var is string
-    
+
   } else {
     data <- list(data)
   }
@@ -236,9 +237,9 @@ waffle_iron_groups <- function (data, mapping, rows = 7, sample_size = 1, na.rm 
     }
     return(grid_data)
   }
-  
+
   res <- map_df(data, ~ gen_grid(.))
-  
+
   if (! is.null(group_mapping)) {
     if (is.numeric(res$group)) {
       res <- res %>%
@@ -247,20 +248,20 @@ waffle_iron_groups <- function (data, mapping, rows = 7, sample_size = 1, na.rm 
         rename(group = real_group)
     }
   }
-  
-  
-  # find the width of each group 
+
+
+  # find the width of each group
   offsets <- res %>%
     group_by(group) %>%
     summarize(width = max(x) + 1) %>%
     mutate(offset = cumsum(width)) %>%
     mutate(offset = lag(offset, default = 0))
-  
+
   res %>%
-    left_join(offsets, by = "group") %>% 
+    left_join(offsets, by = "group") %>%
     mutate(x = x + offset) %>%
     mutate(y = rows - y + 1)
-  
+
 }
 
 
