cd3d9c8e4977b5fb11ca7d79273ec5057dddd315
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-04-06 14:41:36 -0400

    Test dismantle function

---
+++ tests/testthat/test-dismantle.R
@@ -0,0 +1,27 @@
+test_that("dismantle splits a chain of pipes into its components", {
+  output <- "mtcars %>% filter(cyl == 5)" %>%
+    parse_expr() %>%
+    dismantle()
+
+  expect_true(
+    lapply(output, rlang::is_expression) %>%
+      unlist() %>%
+      all()
+  )
+
+  expect_identical(lapply(output, deparse), list("mtcars", "filter(cyl == 5)"))
+})
+
+test_that("dismantle properly splits a chain whose first component is a verb calling the data directly", {
+  output <- "filter(mtcars, cyl == 5)" %>%
+    parse_expr() %>%
+    dismantle()
+
+  expect_true(
+    lapply(output, rlang::is_expression) %>%
+      unlist() %>%
+      all()
+  )
+
+  expect_identical(lapply(output, deparse), list("mtcars", "filter(cyl == 5)"))
+})
