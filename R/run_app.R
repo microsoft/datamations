b3f0a74cf176840a343b8b1be5eac229ea5cf197
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-05-04 17:30:48 -0400

    App skeleton to select data, groups, and summary vars, and to see initial, final data, and a plot

--- R/run_app.R
+++ R/run_app.R
@@ -4,36 +4,51 @@
 #'
 #' @export
 run_app <- function() {
-
   library(dplyr)
   library(palmerpenguins)
 
   ui <- shiny::fluidPage(
+    style = "max-width: 1000px;",
+    shiny::h1("datamations"),
+    shiny::p("Construct a tidyverse pipeline by choosing from the options below. You select a data set, then up to three variables to group by, and finally a variable to summarize and a summary function to apply to it."),
     shiny::fluidRow(
       shiny::column(
-        width = 2,
+        width = 3,
         shiny::selectInput("dataset",
           "Dataset",
           choices = c("penguins", "small_salary_data")
         )
       ),
       shiny::column(
-        width = 2,
+        width = 3,
         shiny::uiOutput("group_by")
       ),
       shiny::column(
-        width = 2,
+        width = 3,
         shiny::uiOutput("summary_var")
       ),
       shiny::column(
-        width = 2,
+        width = 3,
         shiny::uiOutput("summary_function")
       )
     ),
     shiny::fluidRow(
-      shiny::verbatimTextOutput("pipeline"),
-      shiny::uiOutput("datamation_frames") # ,
-      # textOutput("datamation_specs")
+      shiny::h2("Tidyverse pipeline"),
+      shiny::verbatimTextOutput("pipeline")
+    ),
+    shiny::tabsetPanel(
+      shiny::tabPanel(
+        title = "Initial data",
+        reactable::reactableOutput("data_start")
+      ),
+      shiny::tabPanel(
+        title = "Transformed data",
+        reactable::reactableOutput("data_transformed")
+      ),
+      shiny::tabPanel(
+        title = "Plot",
+        shiny::plotOutput("final_plot")
+      )
     )
   )
 
@@ -81,28 +96,86 @@ run_app <- function() {
     })
 
     pipeline <- shiny::reactive({
-      glue::glue("{input$dataset} %>% group_by({paste0(input$group_by, collapse = ', ')}) %>% summarize({input$summary_function} = {input$summary_function}({input$summary_var}, na.rm = TRUE))")
+      pipeline_group_by <- !is.null(input$group_by)
+      if (pipeline_group_by) {
+        glue::glue("{input$dataset} %>% group_by({paste0(input$group_by, collapse = ', ')}) %>% summarize({input$summary_function} = {input$summary_function}({input$summary_var}, na.rm = TRUE))")
+      } else {
+        glue::glue("{input$dataset} %>% summarize({input$summary_function} = {input$summary_function}({input$summary_var}, na.rm = TRUE))")
+      }
     })
 
     output$pipeline <- shiny::renderText({
       pipeline()
     })
 
-    vegalite_specs <- shiny::reactive({
+    datamation <- shiny::reactive({
       shiny::req(pipeline())
       datamation_sanddance(pipeline())
     })
 
-    output$datamation_frames <- shiny::renderUI({
-      vegalite_specs()
+    output$data_start <- reactable::renderReactable({
+      req(input$group_by)
+
+      eval(rlang::parse_expr(input$dataset)) %>%
+        dplyr::select(tidyselect::any_of(c(input$group_by, input$summary_var))) %>%
+        reactable::reactable(defaultPageSize = 5)
     })
 
-    output$datamation_specs <- shiny::renderText({
-      shiny::req(vegalite_specs())
-      specs <- vegalite_specs()
-      map(specs, vegawidget::vw_as_json)
+    pipeline_res <- reactive({
+      eval(rlang::parse_expr(pipeline()))
+    })
+
+    output$data_transformed <- reactable::renderReactable({
+      pipeline_res() %>%
+        dplyr::mutate_if(is.numeric, round, 3) %>%
+        reactable::reactable(defaultPageSize = 5)
+    })
+
+    output$final_plot <- shiny::renderPlot({
+      pipeline_res() %>%
+        create_final_plot(input$group_by, input$summary_var, input$summary_function)
     })
   }
 
   shiny::shinyApp(ui, server)
+}
+
+create_final_plot <- function(data, group_by_var, summary_var, summary_function) {
+
+  n_groups <- length(group_by_var)
+
+  if (n_groups == 3) {
+    data <- data %>%
+      dplyr::rename_at(group_by_var[[3]], ~ paste0("x"))
+  } else {
+    data <- data %>%
+      dplyr::mutate(x = 1)
+  }
+
+  p <- ggplot2::ggplot(data, ggplot2::aes_string(x = "x", y = summary_function))
+
+  if (n_groups == 1) {
+    p <- p +
+      ggplot2::facet_grid(reformulate(group_by_var[[1]], "."))
+  } else if (n_groups %in% 2:3) {
+    p <- p +
+      ggplot2::facet_grid(reformulate(group_by_var[[1]], group_by_var[[2]]))
+  }
+
+  if (n_groups == 3) {
+    p <- p +
+      ggplot2::geom_point(ggplot2::aes_string(color = "x"), size = 5)
+
+    labs <- ggplot2::labs(x = NULL, y = glue::glue("{summary_function}({summary_var})"), color = group_by_var[[3]])
+  } else {
+    p <- p +
+      ggplot2::geom_point(size = 5)
+
+    labs <- ggplot2::labs(x = NULL, y = glue::glue("{summary_function}({summary_var})"))
+  }
+
+  p +
+    labs +
+    ggplot2::theme_minimal(base_size = 24) +
+    ggplot2::theme(axis.text.x = ggplot2::element_blank())
 }
