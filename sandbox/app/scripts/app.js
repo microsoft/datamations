908cb84896c35f9f803bbd75d7a50c8296ffec87
Giorgi <mr.g.ghv@gmail.com>
2021-05-05 16:53:00 +0400

    modified app.js for vegawidget

--- sandbox/app/scripts/app.js
+++ sandbox/app/scripts/app.js
@@ -1,150 +1,135 @@
-function App({
-  specUrls, // if urls, fetch it
-  specs, // if specs as array, use them
-  container = "#vis",
-}) {
-  const frameDuration = 1200;
-  let specsArray, frames;
 
-  async function init() {
-    let files = [];
+const dataUrl = "https://raw.githubusercontent.com/jhofman/datamations/refactor-test/sandbox/specs-for-infogrid/";
+const frameDuration = 1200;
+let specsArray, frames;
 
-    if (specs) {
-      files = specs;
-    } else {
-      files = await loadData();
+async function init(id, specUrls) {
+  let files = await loadData(specUrls);
+
+  for (let i = 0; i < files.length; i++) {
+    const parse = files[i].meta.parse;
+
+    // parsing
+    if (parse === "grid") {
+      files[i] = await getGridSpec(files[i]);
+    } else if (parse === "jitter") {
+      files[i] = await getJitterSpec(files[i]);
     }
 
-    for (let i = 0; i < files.length; i++) {
-      const parse = files[i].meta.parse;
+    const facet = files[i].facet;
+    const spec = files[i].spec;
 
-      // parsing
-      if (parse === "grid") {
-        files[i] = await getGridSpec(files[i]);
-      } else if (parse === "jitter") {
-        files[i] = await getJitterSpec(files[i]);
-      }
+    // fake facets
+    if (facet && spec) {
+      const width = facet.column ? 600 : 200;
+      const height = facet.row ? 600 : 200;
 
-      const facet = files[i].facet;
-      const spec = files[i].spec;
-
-      // fake facets
-      if (facet && spec) {
-        const width = facet.column ? 600 : 200;
-        const height = facet.row ? 600 : 200;
-
-        files[i].data.name = "source";
-        files[i] = await hackFacet(files[i], width, height);
-      }
+      files[i].data.name = "source";
+      files[i] = await hackFacet(files[i], width, height);
     }
+  }
 
-    specsArray = files.map((d) => gemini.vl2vg4gemini(d));
-    frames = [];
+  specsArray = files.map((d) => gemini.vl2vg4gemini(d));
+  frames = [];
 
-    for (let i = 1; i < specsArray.length; i++) {
-      const prev = specsArray[i - 1];
-      const curr = specsArray[i];
+  for (let i = 1; i < specsArray.length; i++) {
+    const prev = specsArray[i - 1];
+    const curr = specsArray[i];
 
-      const recommendations = gemini.recommend(prev, curr, {
-        stageN: 1,
-        scales: {
-          domainDimension: "same",
-        },
-        marks: {
-          change: {
-            data: ["gemini_id"],
-            encode: {
-              update: true,
-              enter: true,
-              exit: true,
-            },
+    const recommendations = gemini.recommend(prev, curr, {
+      stageN: 1,
+      scales: {
+        domainDimension: "same",
+      },
+      marks: {
+        change: {
+          data: ["gemini_id"],
+          encode: {
+            update: true,
+            enter: true,
+            exit: true,
           },
         },
-        totalDuration: frameDuration,
-      });
-
-      recommendations
-        .then((resp) => {
-          frames.push({
-            source: prev,
-            target: curr,
-            gemSpec: resp[0].spec,
-          });
-        })
-        .catch((e) => {});
-    }
-
-    vegaEmbed(container, specsArray[4], {
-      renderer: "svg",
+      },
+      totalDuration: frameDuration,
     });
-  }
 
-  let counter = 0, intervalId;
-
-  function play() {
-    counter = 0;
-    const tick = () => {
-      animateFrame(counter);
-      counter++;
-    };
-    tick();
-
-    if (intervalId) clearInterval(intervalId);
-    intervalId = setInterval(() => {
-      tick();
-      if (counter >= frames.length) {
-        clearInterval(intervalId);
-        counter = 0;
-      }
-    }, frameDuration + 100);
-  }
-
-  async function animateFrame(index) {
-    if (!frames[index]) return;
-
-    const source = frames[index].source;
-    const target = frames[index].target;
-    const spec = frames[index].gemSpec;
-
-    let anim = await gemini.animate(source, target, spec);
-
-    vegaEmbed(container, source, { renderer: "svg" }).then(() => {
-      anim.play(container);
-    });
-  }
-
-  function drawFrame(index) {
-    vegaEmbed(container, specsArray[index], {
-      renderer: "svg",
-    });
-  }
-
-  function loadData() {
-    return Promise.all(
-      specUrls.map((url) => {
-        return d3.json(url);
-      })
-    )
-      .then((files) => {
-        // make adjustments here if needed
-        return files.map((d, i) => {
-          return {
-            ...d,
-            width: 600,
-            height: 600,
-          };
+    recommendations
+      .then((resp) => {
+        frames.push({
+          source: prev,
+          target: curr,
+          gemSpec: resp[0].spec,
         });
       })
-      .catch((e) => {
-        console.error(e.message);
-      });
+      .catch((e) => {});
   }
 
-  init();
-
-  return {
-    play,
-    animateFrame,
-    drawFrame,
-  };
+  vegaEmbed("#" + id, specsArray[0], {
+    renderer: "svg",
+  });
 }
+
+let counter = 0, intervalId;
+
+function play(id) {
+  counter = 0;
+  const tick = () => {
+    animateFrame(counter, id);
+    counter++;
+  };
+  tick();
+
+  if (intervalId) clearInterval(intervalId);
+  intervalId = setInterval(() => {
+    tick();
+    if (counter >= frames.length) {
+      clearInterval(intervalId);
+      counter = 0;
+    }
+  }, frameDuration + 100);
+}
+
+async function animateFrame(index, id) {
+  if (!frames[index]) return;
+
+  const source = frames[index].source;
+  const target = frames[index].target;
+  const spec = frames[index].gemSpec;
+
+  let anim = await gemini.animate(source, target, spec);
+
+  vegaEmbed('#' + id, source, { renderer: "svg" }).then(() => {
+    anim.play('#' + id);
+  });
+}
+
+function loadData(specUrls) {
+  return Promise.all(
+    specUrls.map((url) => {
+      return d3.json(url);
+    })
+  )
+    .then((files) => {
+      // make adjustments here if needed
+      return files.map((d, i) => {
+        return {
+          ...d,
+          width: 600,
+          height: 600,
+        };
+      });
+    })
+    .catch((e) => {
+      console.error(e.message);
+    });
+}
+
+init("vis", [
+  dataUrl + "01-ungrouped.json",
+  dataUrl + "02-column-facet.json",
+  dataUrl + "03-column-row-facet.json",
+  dataUrl + "04-column-row-facet-color.json",
+  dataUrl + "05-jitter.json",
+  dataUrl + "06-summary.json",
+]);
