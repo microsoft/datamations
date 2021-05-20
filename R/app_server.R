e875ee6ce3e67192a0300d56109cd292c76b5cda
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-05-20 08:58:31 -0400

    Split UI and server into separate files

---
+++ R/app_server.R
@@ -0,0 +1,181 @@
+#' The application server
+#'
+#' @param input,output,session Internal parameters for {shiny}.
+#' @noRd
+app_server <- function(input, output, session) {
+  library(dplyr)
+  library(palmerpenguins)
+
+  # Select dataset ----
+
+  dataset <- shiny::reactive({
+    switch(input$dataset,
+           mtcars = datasets::mtcars,
+           penguins = palmerpenguins::penguins,
+           small_salary = datamations::small_salary
+    )
+  })
+
+  # Update group by variables based on dataset ----
+
+  output$group_by <- shiny::renderUI({
+    group_by_vars <- dataset() %>%
+      dplyr::select_if(~ is.factor(.x) | is.character(.x)) %>%
+      names()
+
+    shiny::selectInput(
+      "group_by",
+      "Group by",
+      choices = group_by_vars,
+      selected = group_by_vars[[1]],
+      multiple = TRUE
+    )
+  })
+
+  # Update summary variables based on dataset ----
+
+  output$summary_var <- shiny::renderUI({
+    summarise_vars <- dataset() %>%
+      dplyr::select_if(is.numeric) %>%
+      names()
+
+    shiny::selectInput(
+      "summary_var",
+      "Summary variable",
+      choices = summarise_vars
+    )
+  })
+
+  # Generate pipeline -----
+
+  pipeline <- shiny::eventReactive(input$go, {
+    pipeline_group_by <- !is.null(input$group_by)
+    if (pipeline_group_by) {
+      glue::glue("{input$dataset} %>% group_by({paste0(input$group_by, collapse = ', ')}) %>% summarize({input$summary_function} = {input$summary_function}({input$summary_var}, na.rm = TRUE))")
+    } else {
+      glue::glue("{input$dataset} %>% summarize({input$summary_function} = {input$summary_function}({input$summary_var}, na.rm = TRUE))")
+    }
+  })
+
+  # Evaluate pipeline ----
+
+  pipeline_res <- shiny::reactive({
+    eval(rlang::parse_expr(pipeline()))
+  })
+
+  # Generate datamation -----
+
+  datamation <- shiny::reactive({
+    datamation_sanddance(pipeline(), height = input$height, width = input$width)
+  })
+
+  # Generate tabs of data -----
+
+  # Generate the data, and render DTs for them
+  data_for_tabs <- shiny::reactive({
+    pipeline_group_by <- !is.null(input$group_by)
+
+    supported_tidy_functions <- c("group_by", "summarize")
+
+    fittings <- pipeline() %>%
+      parse_pipeline(supported_tidy_functions)
+
+    data_states <- fittings %>%
+      snake(envir = rlang::global_env())
+
+    data_states_tabs <- vector("list", length = ifelse(pipeline_group_by, 3, 2))
+
+    # State 1: Just the data on its own - full data? Relevant columns only?
+    data_states_tabs[[1]] <- data_states[[1]]
+
+    # No group by
+    # State 2: summarized data
+    if (!pipeline_group_by) {
+      data_states_tabs[[2]] <- data_states[[2]]
+      names(data_states_tabs) <- c("Initial data", glue::glue("{input$summary_function} {input$summary_var}"))
+    }
+
+    # Yes group by
+
+    if (pipeline_group_by) {
+
+      # State 2: Grouped data, ordered by group - full data? Relevant columns only?
+
+      state_2 <- data_states[[2]]
+
+      # Get grouping variables to select and arrange by
+      grouping_vars <- group_vars(state_2)
+      grouping_vars <- rlang::parse_exprs(grouping_vars)
+
+      data_states_tabs[[2]] <- state_2 %>%
+        ungroup() %>%
+        select(!!!grouping_vars, rlang::parse_expr(input$summary_var)) %>%
+        arrange(!!!grouping_vars)
+
+      # State 3: summarised data, ordering columns and data just in case
+      data_states_tabs[[3]] <- data_states[[3]] %>%
+        ungroup() %>%
+        select(!!!grouping_vars, rlang::parse_expr(input$summary_function)) %>%
+        arrange(!!!grouping_vars)
+
+      names(data_states_tabs) <- c("Initial data", glue::glue("Group by {paste0(input$group_by, collapse = ', ')}"), glue::glue("{input$summary_function} {input$summary_var} in each group"))
+    }
+
+    data_states_tabs
+  })
+
+  # Render each of the data tabs into an output
+  shiny::observeEvent(input$go, {
+    purrr::iwalk(data_for_tabs(), function(x, y) {
+      output_name <- paste0("data", y)
+      output[[output_name]] <- reactable::renderReactable(
+        x %>%
+          dplyr::mutate_if(is.numeric, round, 3) %>%
+          reactable::reactable(
+            fullWidth = FALSE,
+            width = 600
+          )
+      )
+    })
+  })
+
+  # Will actually create the tabs and show the output in the next step
+
+  # Outputs -----
+
+  output$pipeline <- shiny::renderText({
+    pipeline()
+  })
+
+  output$datamation <- datamations::renderDatamationSandDance(
+    datamation()
+  )
+
+  # Render UIs
+  shiny::observeEvent(input$go, {
+    output$pipeline_ui <- shiny::renderUI({
+      shiny::fluidRow(
+        shiny::h2("tidyverse pipeline"),
+        shiny::verbatimTextOutput("pipeline")
+      )
+    })
+
+    output$datamation_ui <- shiny::renderUI({
+      shiny::fluidRow(
+        shiny::h2("datamation"),
+        style = paste0("height: ", input$height * 2, "px;"),
+        datamations::datamationSandDanceOutput("datamation")
+      )
+    })
+
+    # Actually add the tabs to the tab panel - so weird!
+    purrr::imap(
+      data_for_tabs(),
+      function(x, y) {
+        output_name <- paste0("data", y)
+        tab <- shiny::tabPanel(y, reactable::reactableOutput(output_name))
+        shiny::appendTab("tab", tab, select = y == "Initial data") # Select it if it's the first tab
+      }
+    )
+  })
+}
