76a66740872fbe0c98f834d017aa33d6fcc0db2f
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-06-14 15:08:18 -0400

    Test plot mapping

---
+++ tests/testthat/test-generate_mapping_from_plot.R
@@ -0,0 +1,33 @@
+test_that("generate_mapping_from_plot can extract x mapping from ggplot", {
+  p <- ggplot2::ggplot(mtcars, ggplot2::aes(x = mpg, y = cyl)) +
+    ggplot2::geom_point()
+  mapping <- generate_mapping_from_plot(p)
+  expect_identical(mapping, list(x = "mpg"))
+  })
+
+test_that("generate_mapping_from_plot can extract x mapping from geo_point", {
+  p <- ggplot2::ggplot(mtcars) +
+    ggplot2::geom_point(ggplot2::aes(x = mpg, y = cyl))
+  mapping <- generate_mapping_from_plot(p)
+  expect_identical(mapping, list(x = "mpg"))
+})
+
+test_that("generate_mapping_from_plot extracts facets", {
+  p <- ggplot2::ggplot(mtcars) +
+    ggplot2::geom_point(ggplot2::aes(x = mpg, y = cyl)) +
+    ggplot2::facet_grid(dplyr::vars(vs), dplyr::vars(am))
+  mapping <- generate_mapping_from_plot(p)
+  expect_identical(mapping, list(x = "mpg", row = "vs", column = "am"))
+
+  p <- ggplot2::ggplot(mtcars) +
+    ggplot2::geom_point(ggplot2::aes(x = mpg, y = cyl)) +
+    ggplot2::facet_grid(dplyr::vars(vs))
+  mapping <- generate_mapping_from_plot(p)
+  expect_identical(mapping, list(x = "mpg", row = "vs"))
+
+  p <- ggplot2::ggplot(mtcars) +
+    ggplot2::geom_point(ggplot2::aes(x = mpg, y = cyl)) +
+    ggplot2::facet_grid(am~vs)
+  mapping <- generate_mapping_from_plot(p)
+  expect_identical(mapping, list(x = "mpg", row = "am", column = "vs"))
+})
