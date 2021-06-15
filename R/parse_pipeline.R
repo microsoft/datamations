4154ce9817b2aaf3a5b0da7f69c4a5b419afd7d1
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-06-15 10:28:48 -0400

    Don't export internal functions

--- R/parse_pipeline.R
+++ R/parse_pipeline.R
@@ -5,8 +5,6 @@
 #' @param pipeline Input pipeline, as a string.
 #' @param supported_tidy_functions Functions that are supported by datamations: \code{group_by} and \code{summarize}/\code{summarise}.
 #'
-#' @export
-#'
 #' @examples
 #' "small_salary %>% group_by(Degree) %>% summarize(mean = mean(Salary))" %>%
 #'   parse_pipeline()
