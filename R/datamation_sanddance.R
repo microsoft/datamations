41f6737b891cf6b035c69c735a485f94b4fbff38
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-07-23 14:30:04 -0400

    Move ggplot2 checking into another function

--- R/datamation_sanddance.R
+++ R/datamation_sanddance.R
@@ -58,36 +58,7 @@ datamation_sanddance <- function(pipeline, envir = rlang::global_env(), pretty =
     }
 
     # Check ggplot2 code for what is supported
-
-    ## Facet wrap
-    contains_facet_wrap <- purrr::map_lgl(ggplot2_fittings, function(x) {
-      stringr::str_detect(x, "facet_wrap")
-    }) %>%
-      any()
-
-    if (contains_facet_wrap) {
-      stop("datamations does not support `facet_wrap()`. Please use `facet_grid()` if you would like to see a faceted datamation.", call. = FALSE)
-    }
-
-    ## geom other than geom_point()
-    contains_geom_not_point <- purrr::map_lgl(ggplot2_fittings, function(x) {
-      stringr::str_detect(x, "geom") & !stringr::str_detect(x, "geom_point")
-    }) %>%
-      any()
-
-    if (contains_geom_not_point) {
-      stop("datamations with ggplot2 code only supports `geom_point()`.", call. = FALSE)
-    }
-
-    ## No geom_point
-    doesnt_contain_geom_point <- purrr::map_lgl(ggplot2_fittings, function(x) {
-      !stringr::str_detect(x, "geom_point")
-    }) %>%
-      all()
-
-    if (doesnt_contain_geom_point) {
-      stop("datamations using ggplot2 code requires a call to `geom_point()`.", call. = FALSE)
-    }
+    parse_ggplot2_code(ggplot2_fittings)
   } else {
     fittings <- full_fittings
   }
--- R/parse_functions.R
+++ R/parse_functions.R
@@ -15,3 +15,40 @@ parse_functions <- function(fittings) {
 
   functions
 }
+
+#' Parse ggplot2 code for validity
+#'
+#' @param ggplot2_fittings List of ggplot2 steps
+#' @noRd
+parse_ggplot2_code <- function(ggplot2_fittings) {
+
+  ## Facet wrap
+  contains_facet_wrap <- purrr::map_lgl(ggplot2_fittings, function(x) {
+    stringr::str_detect(x, "facet_wrap")
+  }) %>%
+    any()
+
+  if (contains_facet_wrap) {
+    stop("datamations does not support `facet_wrap()`. Please use `facet_grid()` if you would like to see a faceted datamation.", call. = FALSE)
+  }
+
+  ## geom other than geom_point()
+  contains_geom_not_point <- purrr::map_lgl(ggplot2_fittings, function(x) {
+    stringr::str_detect(x, "geom") & !stringr::str_detect(x, "geom_point")
+  }) %>%
+    any()
+
+  if (contains_geom_not_point) {
+    stop("datamations with ggplot2 code only supports `geom_point()`.", call. = FALSE)
+  }
+
+  ## No geom_point
+  doesnt_contain_geom_point <- purrr::map_lgl(ggplot2_fittings, function(x) {
+    !stringr::str_detect(x, "geom_point")
+  }) %>%
+    all()
+
+  if (doesnt_contain_geom_point) {
+    stop("datamations using ggplot2 code requires a call to `geom_point()`.", call. = FALSE)
+  }
+}
