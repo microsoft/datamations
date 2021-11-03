49b431253589dff2ec14c2c8b8129688ed2b4d1b
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-11-03 16:51:00 -0400

    Add example and specs

---
+++ sandbox/simpsons_paradox/simpsons_paradox_specs.R
@@ -0,0 +1,52 @@
+library(dplyr)
+devtools::load_all()
+
+jeter_1995 <- data.frame(
+  player = "Derek Jeter",
+  year = 1995,
+  is_hit = c(rep(1, 12), rep(0, 48 - 12))
+)
+
+jeter_1996 <- data.frame(
+  player = "Derek Jeter",
+  year = 1996,
+  is_hit = c(rep(1, 183), rep(0, 582 - 183))
+)
+
+justice_1995 <- data.frame(
+  player = "David Justice",
+  year = 1995,
+  is_hit = c(rep(1, 104), rep(0, 411 - 104))
+)
+
+justice_1996 <- data.frame(
+  player = "David Justice",
+  year = 1996,
+  is_hit = c(rep(1, 45), rep(0, 140 - 45))
+)
+
+df <- bind_rows(
+  jeter_1995,
+  jeter_1996,
+  justice_1995,
+  justice_1996
+) %>%
+  sample_frac(0.3)
+
+# datamation #1:
+# jeter has a higher batting average than justice overall
+"df %>%
+  group_by(player) %>%
+  summarize(
+    batting_average = mean(is_hit)
+  )" %>%
+  datamation_sanddance()
+
+# datamation #2:
+# but justice has a higher batting average than jeter within each year
+"df %>%
+  group_by(player, year) %>%
+  summarize(
+    batting_average = mean(is_hit)
+  )" %>%
+  datamation_sanddance()
