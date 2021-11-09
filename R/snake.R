b2b8300e10d42961fe2b82b9a09fb5e442833c8a
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-11-09 11:58:04 -0500

    Fix small bug, require latest purrr version because of issue with accumulate

--- R/snake.R
+++ R/snake.R
@@ -5,9 +5,11 @@
 #' @noRd
 snake <- function(fittings, envir = parent.frame()) {
   c(
+    # Initial data
     eval(fittings[[1]], envir = envir) %>%
       dplyr::as_tibble() %>%
       list(),
+    # Evaluate each subsequent pipeline step
     suppressMessages(purrr::accumulate(fittings, ~ call("%>%", .x, .y) %>% eval(envir = envir))[-1])
   ) %>%
     purrr::map_if(~ (is.data.frame(.x) || is.vector(.x)) && !dplyr::is_grouped_df(.x), dplyr::as_tibble)
