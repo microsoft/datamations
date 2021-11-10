e6309a88198e0dc697c6042b55f2cb3663f700ab
Chinmay Singh <chsingh@microsoft.com>
2021-11-10 16:56:07 -0500

    Fixed small salary unit test

--- tests/tests/test_small_salary.py
+++ tests/tests/test_small_salary.py
@@ -5,11 +5,13 @@ from datamations import *
 
 def test_small_salary(capsys):
     df = small_salary().df
-    df = DatamationFrame(df, new_property='')
-    
     print(df.groupby('Work').mean())
-
     captured = capsys.readouterr()
 
-    assert "mean" in captured.out
+    assert "Work" in captured.out
+    assert "Salary" in captured.out
+    assert "Academia" in captured.out
+    assert "Industry" in captured.out
+    assert "85.012222" in captured.out
+    assert "91.483761" in captured.out
     
