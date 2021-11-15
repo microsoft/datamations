4637d529b0eda7e30bf1aa3ac3ebd16c24c7fc1b
Sharla Gelfand <sharla.gelfand@gmail.com>
2021-11-15 16:07:22 -0500

    Filter by gemini id instead of true/false

--- R/prep_specs_filter.R
+++ R/prep_specs_filter.R
@@ -17,9 +17,7 @@ prep_specs_filter <- function(.data, mapping, previous_frame, filter_operation, 
   if (!infogrid_frame) {
     # Get previous frame's data, rename datamations_y to the original name
     original_data <- previous_frame[["data"]][["values"]] %>%
-      dplyr::rename_at(Y_FIELD_CHR, ~ paste0(mapping$summary_name)) %>%
-      # Add an ID to keep track of which records are filtered in / out
-      dplyr::mutate(temp_datamations_id = dplyr::row_number())
+      dplyr::rename_at(Y_FIELD_CHR, ~ paste0(mapping$summary_name))
 
     filter_operation_combined <- glue::glue_collapse(filter_operation, sep = "& ")
 
@@ -27,19 +25,24 @@ prep_specs_filter <- function(.data, mapping, previous_frame, filter_operation, 
       # Filter the data
       dplyr::filter(eval(rlang::parse_expr(filter_operation_combined))) %>%
       # Add a flag that the ones left are kept
-      dplyr::mutate(datamations_filter = 1)
+      dplyr::mutate(datamations_filter = TRUE)
 
-    # Reconstruct the original data, with a flag of 1 / 0 for filtered in / out
+    # Reconstruct the original data, with a flag of TRUE / FALSE for filtered in / out
     original_data_with_filter_flag <- original_data %>%
       dplyr::left_join(filtered_data, by = names(original_data)) %>%
-      dplyr::mutate(datamations_filter = dplyr::coalesce(datamations_filter, 0)) %>%
+      dplyr::mutate(datamations_filter = dplyr::coalesce(datamations_filter, FALSE)) %>%
       # Rename Y_FIELD in the end, since it's the name used in the spec
       dplyr::rename_at(mapping$summary_name, ~ paste0(Y_FIELD_CHR))
+
+    filter_ids <- original_data_with_filter_flag %>%
+      dplyr::ungroup() %>%
+      dplyr::filter(.data$datamations_filter) %>%
+      dplyr::pull(.data$gemini_id)
 
     # Reconstruct the previous spec, replacing the data and adding a filter transform
     spec <- previous_frame
     spec[["data"]][["values"]] <- original_data_with_filter_flag
-    spec[["transform"]] <- list(list(filter = "datum.datamations_filter == 1"))
+    spec[["transform"]] <- list(list(filter = list(field = "gemini_id", oneOf = filter_ids)))
 
     # Update title of frame
     spec[["meta"]][["description"]] <- glue::glue("Filter {filter_operation}",
