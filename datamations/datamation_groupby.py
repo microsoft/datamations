3ef675206a67a072eb3f90b1856478a5c5acb5a7
Chinmay Singh <chsingh@microsoft.com>
2021-11-24 20:03:47 +0000

    mean for each group

--- datamations/datamation_groupby.py
+++ datamations/datamation_groupby.py
@@ -175,8 +175,90 @@ class DatamationGroupBy(pd.core.groupby.generic.DataFrameGroupBy):
         }
         specs_list.append(spec)
 
+        spec_encoding = {
+            "x": {
+                "field": "datamations_x",
+                "type": "quantitative",
+                "axis": {
+                "values": [1, 2],
+                "labelExpr": "round(datum.label) == 1 ? 'Masters' : 'PhD'",
+                "labelAngle": -90
+                },
+                "title": "Degree",
+                "scale": {
+                "domain": [0.5, 2.5]
+                }
+            },
+            "y": {
+                "field": "datamations_y",
+                "type": "quantitative",
+                "title": "mean(Salary)",
+                "scale": {
+                "domain": [81.9445013836958, 94.0215112566948]
+                }
+            },
+            "tooltip": [
+                {
+                "field": "datamations_y_tooltip",
+                "type": "quantitative",
+                "title": "mean(Salary)"
+                },
+                {
+                "field": "Degree",
+                "type": "nominal"
+                }
+            ]
+        }
+
+        values = []
+        
+        id = 1
+        for i in range(len(self.states[0])):
+            if self.states[0]['Degree'][i] == 'PhD':
+                continue
+            values.append({
+                "gemini_id": id,
+                "Degree": self.states[0]['Degree'][i],
+                "datamations_x": 1 if self.states[0]['Degree'][i] == 'Masters'  else 2,
+                "datamations_y": 90.2263340061763,
+                "datamations_y_tooltip": 90.2263340061763,
+            })
+            id = id + 1
+
+        for i in range(len(self.states[0])):
+            if self.states[0]['Degree'][i] == 'Masters':
+                continue
+            values.append({
+                "gemini_id": id,
+                "Degree": self.states[0]['Degree'][i],
+                "datamations_x": 1 if self.states[0]['Degree'][i] == 'Masters'  else 2,
+                "datamations_y": 88.2456061263219,
+                "datamations_y_tooltip": 88.2456061263219
+            })
+            id = id + 1
+
+        spec = {
+            "height": height,
+            "width": width,
+            "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+            "data": {
+                "values": values
+            },
+            "meta": { 
+                "axes": False,
+                "description": "Plot mean Salary of each group"
+            },
+            "mark": {
+                "type": "point",
+                "filled": True,
+                "strokeWidth": 1
+            },
+            "encoding": spec_encoding
+        }
+        specs_list.append(spec)
+
         script_dir = os.path.dirname( __file__ )
         specs_file = open(os.path.join(script_dir, '../sandbox/specs_for_python/raw_spec.json'), 'r')
         specs = json.load(specs_file)
-        specs_list = specs_list + specs[3:]
+        specs_list = specs_list + specs[4:]
         return specs_list
