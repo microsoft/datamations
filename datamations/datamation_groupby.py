42a0fa1238052957f389363073a0a5bba496c822
Chinmay Singh <chsingh@microsoft.com>
2021-11-24 20:43:48 +0000

    plot with error bars, zoomed in

--- datamations/datamation_groupby.py
+++ datamations/datamation_groupby.py
@@ -2,7 +2,7 @@
 #
 # Create a subclass from a pandas DataFrame.
 #
-import os, json
+import json
 import pandas as pd
 from . import datamation_frame
 
@@ -257,8 +257,272 @@ class DatamationGroupBy(pd.core.groupby.generic.DataFrameGroupBy):
         }
         specs_list.append(spec)
 
-        script_dir = os.path.dirname( __file__ )
-        specs_file = open(os.path.join(script_dir, '../sandbox/specs_for_python/raw_spec.json'), 'r')
-        specs = json.load(specs_file)
-        specs_list = specs_list + specs[4:]
+        layer = [
+            {
+                "mark": "errorbar",
+                "encoding": {
+                    "x": {
+                        "field": "datamations_x",
+                        "type": "quantitative",
+                        "axis": {
+                        "values": [1, 2],
+                        "labelExpr": "round(datum.label) == 1 ? 'Masters' : 'PhD'",
+                        "labelAngle": -90
+                        },
+                        "title": "Degree",
+                        "scale": {
+                        "domain": [0.5, 2.5]
+                        }
+                    },
+                    "y": {
+                        "field": "datamations_y_raw",
+                        "type": "quantitative",
+                        "title": "mean(Salary)",
+                        "scale": {
+                        "domain": [81.9445013836958, 94.0215112566948]
+                        }
+                    },
+                    "tooltip": [
+                        {
+                        "field": "datamations_y_tooltip",
+                        "type": "quantitative",
+                        "title": "mean(Salary)"
+                        },
+                        {
+                        "field": "Upper",
+                        "type": "nominal",
+                        "title": "mean(Salary) + standard error"
+                        },
+                        {
+                        "field": "Lower",
+                        "type": "nominal",
+                        "title": "mean(Salary) - standard error"
+                        },
+                        {
+                        "field": "Degree",
+                        "type": "nominal"
+                        }
+                    ]
+                }
+            },
+            {
+                "mark": {
+                "type": "point",
+                "filled": True,
+                "strokeWidth": 1
+                },
+                "encoding": {
+                    "x": {
+                        "field": "datamations_x",
+                        "type": "quantitative",
+                        "axis": {
+                        "values": [1, 2],
+                        "labelExpr": "round(datum.label) == 1 ? 'Masters' : 'PhD'",
+                        "labelAngle": -90
+                        },
+                        "title": "Degree",
+                        "scale": {
+                        "domain": [0.5, 2.5]
+                        }
+                    },
+                    "y": {
+                        "field": "datamations_y",
+                        "type": "quantitative",
+                        "title": "mean(Salary)",
+                        "scale": {
+                        "domain": [81.9445013836958, 94.0215112566948]
+                        }
+                    },
+                    "tooltip": [
+                        {
+                        "field": "datamations_y_tooltip",
+                        "type": "quantitative",
+                        "title": "mean(Salary)"
+                        },
+                        {
+                        "field": "Upper",
+                        "type": "nominal",
+                        "title": "mean(Salary) + standard error"
+                        },
+                        {
+                        "field": "Lower",
+                        "type": "nominal",
+                        "title": "mean(Salary) - standard error"
+                        },
+                        {
+                        "field": "Degree",
+                        "type": "nominal"
+                        }
+                    ]
+                }
+            }
+        ]
+
+        values = []
+        
+        id = 1
+        for i in range(len(self.states[0])):
+            if self.states[0]['Degree'][i] == 'PhD':
+                continue
+            values.append({
+                "gemini_id": id,
+                "Degree": self.states[0]['Degree'][i],
+                "datamations_x": 1 if self.states[0]['Degree'][i] == 'Masters'  else 2,
+                "datamations_y": 90.2263340061763,
+                "datamations_y_raw": self.states[0]['Salary'][i],
+                "datamations_y_tooltip": 90.2263340061763,
+                "Lower": 89.9152330064628,
+                "Upper": 90.5374350058899
+            })
+            id = id + 1
+
+        for i in range(len(self.states[0])):
+            if self.states[0]['Degree'][i] == 'Masters':
+                continue
+            values.append({
+                "gemini_id": id,
+                "Degree": self.states[0]['Degree'][i],
+                "datamations_x": 1 if self.states[0]['Degree'][i] == 'Masters'  else 2,
+                "datamations_y": 88.2456061263219,
+                "datamations_y_raw": self.states[0]['Salary'][i],
+                "datamations_y_tooltip": 88.2456061263219,
+                "Lower": 87.5297479964919,
+                "Upper": 88.961464256152
+            })
+            id = id + 1
+
+        spec = {
+            "height": height,
+            "width": width,
+            "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+            "data": {
+                "values": values
+            },
+            "meta": { 
+                "axes": False,
+                "description": "Plot mean Salary of each group, with errorbar"
+            },
+            "layer": layer
+        }
+
+        specs_list.append(spec)
+
+        layer = [
+            {
+                "mark": "errorbar",
+                "encoding": {
+                    "x": {
+                        "field": "datamations_x",
+                        "type": "quantitative",
+                        "axis": {
+                        "values": [1, 2],
+                        "labelExpr": "round(datum.label) == 1 ? 'Masters' : 'PhD'",
+                        "labelAngle": -90
+                        },
+                        "title": "Degree",
+                        "scale": {
+                        "domain": [0.5, 2.5]
+                        }
+                    },
+                    "y": {
+                        "field": "datamations_y_raw",
+                        "type": "quantitative",
+                        "title": "mean(Salary)",
+                        "scale": {
+                        "domain": [87.5297479964919, 90.5374350058899]
+                        }
+                    },
+                    "tooltip": [
+                        {
+                        "field": "datamations_y_tooltip",
+                        "type": "quantitative",
+                        "title": "mean(Salary)"
+                        },
+                        {
+                        "field": "Upper",
+                        "type": "nominal",
+                        "title": "mean(Salary) + standard error"
+                        },
+                        {
+                        "field": "Lower",
+                        "type": "nominal",
+                        "title": "mean(Salary) - standard error"
+                        },
+                        {
+                        "field": "Degree",
+                        "type": "nominal"
+                        }
+                    ]
+                }
+            },
+            {
+                "mark": {
+                "type": "point",
+                "filled": True,
+                "strokeWidth": 1
+                },
+                "encoding": {
+                    "x": {
+                        "field": "datamations_x",
+                        "type": "quantitative",
+                        "axis": {
+                        "values": [1, 2],
+                        "labelExpr": "round(datum.label) == 1 ? 'Masters' : 'PhD'",
+                        "labelAngle": -90
+                        },
+                        "title": "Degree",
+                        "scale": {
+                        "domain": [0.5, 2.5]
+                        }
+                    },
+                    "y": {
+                        "field": "datamations_y",
+                        "type": "quantitative",
+                        "title": "mean(Salary)",
+                        "scale": {
+                        "domain": [87.5297479964919, 90.5374350058899]
+                        }
+                    },
+                    "tooltip": [
+                        {
+                        "field": "datamations_y_tooltip",
+                        "type": "quantitative",
+                        "title": "mean(Salary)"
+                        },
+                        {
+                        "field": "Upper",
+                        "type": "nominal",
+                        "title": "mean(Salary) + standard error"
+                        },
+                        {
+                        "field": "Lower",
+                        "type": "nominal",
+                        "title": "mean(Salary) - standard error"
+                        },
+                        {
+                        "field": "Degree",
+                        "type": "nominal"
+                        }
+                    ]
+                }
+            }
+        ]
+
+        spec = {
+            "height": height,
+            "width": width,
+            "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+            "data": {
+                "values": values
+            },
+            "meta": { 
+                "axes": False,
+                "description": "Plot mean Salary of each group, with errorbar, zoomed in"
+            },
+            "layer": layer
+        }
+
+        specs_list.append(spec)
+
+        specs_list = specs_list
         return specs_list
