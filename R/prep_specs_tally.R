bdb999bcd66f877279df02c07c1bd10ecd310976
willdebras <wtb3na@virginia.edu>
2022-01-13 17:35:28 -0600

    beginning work on tally functionality

---
+++ R/prep_specs_tally.R
@@ -0,0 +1,12 @@
+#' Generate specs of data for count step of datamation
+#'
+#' @param .data Input data
+#' @param mapping A list that describes mapping for the datamations, including x and y variables, summary variable and operation, variables used in facets and in colors, etc. Generated in \code{datamation_sanddance} using \code{generate_mapping}.
+#' @inheritParams datamation_sanddance
+#' @inheritParams prep_specs_data
+#' @noRd
+prep_specs_tally <- function(.data, mapping, toJSON = TRUE, pretty = TRUE, height = 300, width = 300) {
+
+#TODO, ADD FUNCTIONALITY FOR TALLY
+
+}
