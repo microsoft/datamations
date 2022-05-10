735e39ac6f8526ba410748b2ad7fe5802098ca14
Chinmay Singh <chsingh@microsoft.com>
2022-05-10 10:51:19 -0400

    fixed zoomed in domains

--- inst/htmlwidgets/js/src/scripts/datamation-sanddance.js
+++ inst/htmlwidgets/js/src/scripts/datamation-sanddance.js
@@ -957,9 +957,9 @@ function prep_specs_summarize(states, groupby, summarize, output) {
   }
 
   // Show the summarized values along with error bars, zoomed in
+  var min_array = [];
+  var max_array = [];
   if (groupby.length > 2) {
-    var min_array = [];
-    var max_array = [];
     for (group of groups) {
       for (subgroup of subgroups) {
         for (var l3group of l3groups) {
@@ -990,27 +990,59 @@ function prep_specs_summarize(states, groupby, summarize, output) {
     });
     var domain = [_.round(Math.min(...min_array), 13), _.round(Math.max(...max_array), 13)];
   } else if (groupby.length > 1) {
-    domain = [
-      _.round(
-        Math.min(
-          output[groups[0]][subgroups[0]] - _error[[groups[0], subgroups[0]].join(",")],
-          output[groups[1]][subgroups[1]] - _error[[groups[0], subgroups[0]].join(",")]
-        ),
-        13
-      ),
-      _.round(
-        Math.max(
-          output[groups[1]][subgroups[0]] + _error[[groups[1], subgroups[0]].join(",")],
-          output[groups[1]][subgroups[1]] + _error[[groups[1], subgroups[1]].join(",")]
-        ),
-        13
-      ),
-    ];
+    for (group of groups) {
+      for (subgroup of subgroups) {
+        if (output[group][subgroup] && !isNaN(output[group][subgroup])) {
+          if (operation == "mean") {
+            min_array.push(output[group][subgroup] - _error[[group, subgroup].join(",")]);
+          }
+          else {
+            min_array.push(output[group][subgroup]);
+          }
+        }
+        if (output[group][subgroup] && !isNaN(output[group][subgroup])) {
+          if (operation == "mean") {
+            max_array.push(output[group][subgroup] + _error[[group, subgroup].join(",")]);
+          }
+          else {
+            max_array.push(output[group][subgroup]);
+          }
+        }
+      }
+    }
+    min_array = min_array.filter((item) => {
+      return !isNaN(item);
+    });
+    max_array = max_array.filter((item) => {
+      return !isNaN(item);
+    });
+    domain = [_.round(Math.min(...min_array), 13), _.round(Math.max(...max_array), 13)];
   } else {
-    domain = [
-      _.round(Math.min(output[groups[0]] - _error[groups[0]], output[groups[1]] - _error[groups[1]]), 13),
-      _.round(Math.max(output[groups[0]] + _error[groups[0]], output[groups[1]] + _error[groups[1]]), 13),
-    ];
+    for (group of groups) {
+      if (output[group] && !isNaN(output[group])) {
+        if (operation == "mean") {
+          min_array.push(output[group] - _error[group]);
+        }
+        else {
+          min_array.push(output[group]);
+        }
+      }
+      if (output[group] && !isNaN(output[group])) {
+        if (operation == "mean") {
+          max_array.push(output[group] + _error[group]);
+        }
+        else {
+          max_array.push(output[group]);
+        }
+      }
+    }
+    min_array = min_array.filter((item) => {
+      return !isNaN(item);
+    });
+    max_array = max_array.filter((item) => {
+      return !isNaN(item);
+    });
+    domain = [_.round(Math.min(...min_array), 13), _.round(Math.max(...max_array), 13)];
   }
 
   y_encoding = {
